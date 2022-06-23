0457d35813b7dc4240bf51c30c6b8c108584ef3a
dilan504 <dch19940@gmail.com>
2022-06-23 17:03:30 -0400

    Inicializacion de proyecto

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+const {Builder, By, Key} = require('selenium-webdriver');
+// const firefox = require('selenium-webdriver/firefox');   // En caso de una ruta diferente de firefox descomentar esta linea
+
+(async function facebookLogin() {
+    // const binary = "/opt/firefox/firefox" // En caso de tener firefox en una ruta distinta a la que es por defecto cambiar esta variable
+  
+    let driver = await new Builder().forBrowser('firefox').setFirefoxOptions().build();  
+    // let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().setBinary(binary)).build();   // En caso de una ruta diferente de firefox descomentar esta linea
+    try {
+      await driver.get('https://www.twitter.com/signup');
+      //await driver.findElement(By.name('email')).sendKeys('dch19940@gmail.com');
+      //await driver.findElement(By.name('pass')).sendKeys('P@ssw0rd', Key.RETURN);
+      await driver.sleep(5 * 1000)
+    } finally {
+      await driver.quit();
+    }
+  })();
