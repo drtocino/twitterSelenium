3d295b5094fd4a77fff57aabd62d969a3512215b
dilan504 <dch19940@gmail.com>
2022-06-23 17:49:14 -0400

    Login en twitter con numero de telefono

--- tests/test.js
+++ tests/test.js
@@ -7,10 +7,15 @@ const {Builder, By, Key} = require('selenium-webdriver');
     let driver = await new Builder().forBrowser('firefox').setFirefoxOptions().build();  
     // let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().setBinary(binary)).build();   // En caso de una ruta diferente de firefox descomentar esta linea
     try {
-      await driver.get('https://www.twitter.com/signup');
-      //await driver.findElement(By.name('email')).sendKeys('dch19940@gmail.com');
-      //await driver.findElement(By.name('pass')).sendKeys('P@ssw0rd', Key.RETURN);
-      await driver.sleep(5 * 1000)
+      await driver.get('https://www.twitter.com/login');
+      await driver.sleep(10 * 1000)
+      await driver.findElement(By.name('text')).sendKeys("60744773");
+      await driver.findElement(By.xpath('/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')).click();
+      await driver.sleep(10 * 1000)
+      await driver.findElement(By.name('password')).sendKeys("P@ssw0rd");
+      await driver.findElement(By.xpath('/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')).click();
+      await driver.sleep(10 * 1000)
+
     } finally {
       await driver.quit();
     }
